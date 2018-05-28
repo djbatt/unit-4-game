12b947011b1426de64cfb2b928f27e4a7e07c731
djbatt <djbatt@live.com>
2018-05-28 13:57:30 -0400

    added a way to update the page, added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,30 @@ $(document).ready(function() {
     var crystalCollector = {
         currentScore: 0,
         numberGoal: Math.floor(Math.random() * 120) + 19,
-        crystalValue,
         wins: 0,
         losses: 0,
+        gemArray: [],
 
         pageSetup: function() {
+
             $("#randomNumberChoice").text(crystalCollector.numberGoal);
             $("#yourWins").text(crystalCollector.wins);
             $("#yourLosses").text(crystalCollector.losses);
             $("#yourCurrentScore").text(crystalCollector.currentScore);
+
+            for (var i = 0; i < 4; i++) {
+                crystalCollector.gemArray.push(Math.floor(Math.random() * 12) + 1);
+            }
         },
 
-        userGuess: function() {
-            var crystalValue = Math.floor(Math.random() * 12) + 1;
-            console.log(crystalValue);
+        updatePage: function() {
+            $("#randomNumberChoice").text(crystalCollector.numberGoal);
+            $("#yourWins").text(crystalCollector.wins);
+            $("#yourLosses").text(crystalCollector.losses);
+            $("#yourCurrentScore").text(crystalCollector.currentScore);
         }
     }
+    
+    crystalCollector.pageSetup();
 
-    crystalCollector.pageSetup()
 });
