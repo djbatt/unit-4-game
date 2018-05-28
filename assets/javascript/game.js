d7244699315474a640bb5b706978a70850cc2cfc
djbatt <djbatt@live.com>
2018-05-28 14:32:20 -0400

    added userguessing, and win checking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,15 @@
 $(document).ready(function() {
     var crystalCollector = {
         currentScore: 0,
-        numberGoal: Math.floor(Math.random() * 120) + 19,
+        numberGoal: 0,
         wins: 0,
         losses: 0,
         gemArray: [],
 
         pageSetup: function() {
+            crystalCollector.gemArray = [];
+            crystalCollector.currentScore = 0;
+            crystalCollector.numberGoal = Math.floor(Math.random() * 120) + 19;
 
             $("#randomNumberChoice").text(crystalCollector.numberGoal);
             $("#yourWins").text(crystalCollector.wins);
@@ -17,6 +20,8 @@ $(document).ready(function() {
             for (var i = 0; i < 4; i++) {
                 crystalCollector.gemArray.push(Math.floor(Math.random() * 12) + 1);
             }
+
+            console.log(crystalCollector.gemArray);
         },
 
         updatePage: function() {
@@ -24,9 +29,49 @@ $(document).ready(function() {
             $("#yourWins").text(crystalCollector.wins);
             $("#yourLosses").text(crystalCollector.losses);
             $("#yourCurrentScore").text(crystalCollector.currentScore);
+        },
+
+        checkWin: function() {
+            if (crystalCollector.currentScore === crystalCollector.numberGoal) {
+                crystalCollector.wins++
+                crystalCollector.pageSetup();
+            }
+
+            else if (crystalCollector.currentScore > crystalCollector.numberGoal) {
+                crystalCollector.losses++
+                crystalCollector.pageSetup();
+            }
+        },
+
+        userGuess: function() {
+            $(".sapphire").click(function () {
+                var crystal = crystalCollector.gemArray[0];
+                crystalCollector.currentScore = crystalCollector.currentScore + crystalCollector.gemArray[0];
+                crystalCollector.updatePage();
+                crystalCollector.checkWin();
+            });
+            $(".ruby").click(function () {
+                var crystal = crystalCollector.gemArray[1];
+                crystalCollector.currentScore = crystalCollector.currentScore + crystalCollector.gemArray[1];
+                crystalCollector.updatePage();
+                crystalCollector.checkWin();
+            });
+            $(".emerald").click(function () {
+                var crystal = crystalCollector.gemArray[2];
+                crystalCollector.currentScore = crystalCollector.currentScore + crystalCollector.gemArray[2];
+                crystalCollector.updatePage();
+                crystalCollector.checkWin();
+            });
+            $(".diamond").click(function () {
+                var crystal = crystalCollector.gemArray[3];
+                crystalCollector.currentScore = crystalCollector.currentScore + crystalCollector.gemArray[3];
+                crystalCollector.updatePage();
+                crystalCollector.checkWin();
+            });
         }
     }
     
+    crystalCollector.userGuess();
     crystalCollector.pageSetup();
 
 });
